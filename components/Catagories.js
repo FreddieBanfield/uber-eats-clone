2dad33ef47a164f9ca386f37f7c57f3d36e7af79
Fred <freddiebanfield@googlemail.com>
2022-02-24 21:29:18 -0800

    Added Catagory bar

--- components/Catagories.js
+++ components/Catagories.js
@@ -1,10 +1,62 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import React from "react";
+
+const items = [
+  {
+    image: require("../assets/images/shopping-bag.png"),
+    text: "Pick-up",
+  },
+  {
+    image: require("../assets/images/soft-drink.png"),
+    text: "Soft Drinks",
+  },
+  {
+    image: require("../assets/images/bread.png"),
+    text: "Bakery Items",
+  },
+  {
+    image: require("../assets/images/fast-food.png"),
+    text: "Fast Foods",
+  },
+  {
+    image: require("../assets/images/deals.png"),
+    text: "Deals",
+  },
+  {
+    image: require("../assets/images/coffee.png"),
+    text: "Coffee & Tea",
+  },
+  {
+    image: require("../assets/images/desserts.png"),
+    text: "Desserts",
+  },
+];
 
 export default function Catagories() {
   return (
-    <View>
-      <Text>Catagories</Text>
+    <View
+      style={{
+        marginTop: 5,
+        backgroundColor: "white",
+        paddingVertical: 10,
+        paddingLeft: 20,
+      }}
+    >
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {items.map((item, index) => (
+          <View key={index} style={{ alignItems: "center", marginRight: 30 }}>
+            <Image
+              source={item.image}
+              style={{
+                width: 50,
+                height: 40,
+                resizeMode: "contain",
+              }}
+            />
+            <Text style={{ fontSize: 13, fontWeight: "900" }}>{item.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
