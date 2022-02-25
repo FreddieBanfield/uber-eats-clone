fa9565f7fefe57f24f065e6290c875c332229c22
Fred <freddiebanfield@googlemail.com>
2022-02-24 20:02:51 -0800

    Created HeaderTabs.js

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import { View, Text } from "react-native";
+import Home from "./screens/Home.js";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Uber Eats Clone</Text>
-    </View>
-  );
+  return <Home />;
 }
---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+import React from "react";
+import HeaderTabs from "../components/HeaderTabs.js";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <HeaderTabs />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
