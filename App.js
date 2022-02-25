7a7dc951078ee03f202315e62c829e8de64877fd
Fred <freddiebanfield@googlemail.com>
2022-02-24 19:04:12 -0800

    First Commit

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text>Universal React with Expo</Text>
+      <Text>Uber Eats Clone</Text>
     </View>
   );
 }
