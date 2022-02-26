0c1b7feae48e93bddbb36222dab2b9d6577ee8ae
Fred <freddiebanfield@googlemail.com>
2022-02-25 16:51:44 -0800

    Added RestaurantItem.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import { StyleSheet, Text, SafeAreaView, View, ScrollView } from "react-native";
 import React from "react";
 import HeaderTabs from "./../components/HeaderTabs.js";
 import SearchBar from "./../components/SearchBar.js";
 import Catagories from "./../components/Catagories";
+import RestaurantItem from "./../components/RestaurantItem";
 
 const Home = () => {
   return (
@@ -11,7 +12,10 @@ const Home = () => {
         <HeaderTabs />
         <SearchBar />
       </View>
-      <Catagories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Catagories />
+        <RestaurantItem />
+      </ScrollView>
     </SafeAreaView>
   );
 };
